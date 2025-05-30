6807d735ef76dac281df38c0b6490c121858703a
gopu2311 <157279850+gopu2311@users.noreply.github.com>
2025-05-30 13:44:59 +0530

    Update rename_file.py

--- rename_file.py
+++ rename_file.py
@@ -1,9 +1,9 @@
 import os 
 def rename_files(folder_path):
-    for filename is os.lisdir(folder_path):
-        if filename.endwith(".txt"):
-        base = os.path.splitpath(filename)[0]
-        old_path=os.path.join(folder_path,filename)
-        new_path=os.path.join(folder_path, base + ".log")
-        os.rename(old_path,new_path)
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+          base = os.path.splitext(filename)[0]
+          old_path=os.path.join(folder_path,filename)
+          new_path=os.path.join(folder_path, base + ".log")
+          os.rename(old_path,new_path)
 rename_files("folder_path")
